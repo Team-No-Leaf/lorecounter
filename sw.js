7fb39d116bfb5aca7ed5295fdfc00ea8c5f3bb85
Sorgen <sjoerd97koopman@me.com>
2026-06-29 10:27:10 +0200

    Rename app to Sorgen's Inkwell

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "lorcana-scorekeeper-v21";
+const CACHE_NAME = "lorcana-scorekeeper-v22";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=21",
-  "./app.js?v=21",
+  "./styles.css?v=22",
+  "./app.js?v=22",
   "./manifest.webmanifest",
   "./icon.svg",
   "./assets/ink/dlc_ink_amber.png",
