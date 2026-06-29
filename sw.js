1aeb41185e1c16db58a89d4cdc1b3f888b464f8c
Sorgen <sjoerd97koopman@me.com>
2026-06-29 10:25:10 +0200

    Publish Lorcana scorekeeper app

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "lorcana-scorekeeper-v21";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css?v=21",
+  "./app.js?v=21",
+  "./manifest.webmanifest",
+  "./icon.svg",
+  "./assets/ink/dlc_ink_amber.png",
+  "./assets/ink/dlc_ink_amethyst.png",
+  "./assets/ink/dlc_ink_emerald.png",
+  "./assets/ink/dlc_ink_ruby.png",
+  "./assets/ink/dlc_ink_sapphire.png",
+  "./assets/ink/dlc_ink_steel.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) =>
+      cached || fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+    )
+  );
+});
