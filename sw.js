5ec02f26d706ef7e361838c9a9426b387e266f1c
Sorgen <sjoerd97koopman@me.com>
2026-07-06 12:48:45 +0200

    Use official card size for proxy PDFs

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "lorcana-scorekeeper-v38";
+const CACHE_NAME = "lorcana-scorekeeper-v39";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=38",
-  "./app.js?v=38",
-  "./manifest.webmanifest?v=38",
+  "./styles.css?v=39",
+  "./app.js?v=39",
+  "./manifest.webmanifest?v=39",
   "./proxies/",
   "./proxies/index.html",
   "./icon.svg",
