93e9c623540b421578f8c8bb633d16a096bd4152
Sorgen <sjoerd97koopman@me.com>
2026-07-06 13:48:07 +0200

    Fix proxy willpower and ability spacing

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "lorcana-scorekeeper-v47";
+const CACHE_NAME = "lorcana-scorekeeper-v48";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=47",
-  "./app.js?v=47",
-  "./manifest.webmanifest?v=47",
+  "./styles.css?v=48",
+  "./app.js?v=48",
+  "./manifest.webmanifest?v=48",
   "./icon.svg",
   "./assets/icons/illuminary-favicon.png",
   "./assets/icons/illuminary-180.png",
