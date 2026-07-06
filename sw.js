c811203b437b9398cb73b194b3cbe0a70dac024e
Sorgen <sjoerd97koopman@me.com>
2026-07-06 12:54:02 +0200

    Add proxy PDF lore and inkable markers

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "lorcana-scorekeeper-v39";
+const CACHE_NAME = "lorcana-scorekeeper-v40";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=39",
-  "./app.js?v=39",
-  "./manifest.webmanifest?v=39",
+  "./styles.css?v=40",
+  "./app.js?v=40",
+  "./manifest.webmanifest?v=40",
   "./proxies/",
   "./proxies/index.html",
   "./icon.svg",
