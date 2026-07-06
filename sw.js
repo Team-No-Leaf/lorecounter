35b086592e483de3cb7467896881a2bdc77ece11
Sorgen <sjoerd97koopman@me.com>
2026-07-06 14:55:54 +0200

    Add Ko-fi support button

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "lorcana-scorekeeper-v55";
+const CACHE_NAME = "lorcana-scorekeeper-v56";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=55",
-  "./app.js?v=55",
-  "./manifest.webmanifest?v=55",
+  "./styles.css?v=56",
+  "./app.js?v=56",
+  "./manifest.webmanifest?v=56",
   "./icon.svg",
   "./assets/icons/illuminary-favicon.png",
   "./assets/icons/illuminary-180.png",
