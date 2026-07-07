8699590414969c7c107be63c521ea9f32972dc31
Sorgen <sjoerd97koopman@me.com>
2026-07-07 13:08:55 +0200

    Invert cost icons in dark mode

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "lorcana-scorekeeper-v77";
+const CACHE_NAME = "lorcana-scorekeeper-v78";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=67",
-  "./app.js?v=77",
+  "./app.js?v=78",
   "./manifest.webmanifest?v=67",
   "./proxies/",
   "./proxies/index.html",
