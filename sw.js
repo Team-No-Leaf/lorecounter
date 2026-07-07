65fac84d016f62c72ef94dee5feb8bcab7485683
Sorgen <sjoerd97koopman@me.com>
2026-07-07 14:24:01 +0200

    Use new Inkwell logo assets

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "lorcana-scorekeeper-v80";
+const CACHE_NAME = "lorcana-scorekeeper-v81";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=67",
-  "./app.js?v=80",
+  "./app.js?v=81",
   "./manifest.webmanifest?v=67",
   "./proxies/",
   "./proxies/index.html",
