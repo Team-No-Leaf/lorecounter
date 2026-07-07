21ea2ab5d76b47e40e3a7d67a9c65c038dd8576d
Sorgen <sjoerd97koopman@me.com>
2026-07-07 13:22:28 +0200

    Use icon-only ink filter buttons

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "lorcana-scorekeeper-v79";
+const CACHE_NAME = "lorcana-scorekeeper-v80";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=67",
-  "./app.js?v=79",
+  "./app.js?v=80",
   "./manifest.webmanifest?v=67",
   "./proxies/",
   "./proxies/index.html",
